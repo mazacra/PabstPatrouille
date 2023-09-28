6d48ca426d4a7f84fe85860292b771cb7cab3c44
MAubin <mazacra12345@gmail.com>
2023-09-28 11:23:43 -0400

    Debut code trajet

--- PabstPatrouille/src/main.cpp
+++ PabstPatrouille/src/main.cpp
@@ -1,13 +1,104 @@
-#include <Arduino.h>
 #include <LibRobus.h>
 
+int x,y,angle;
 
-void setup() {
-  BoardInit(); //Init libraire LibRobus
+void setup(){
+  BoardInit();
 
-  //Autre setup (Variable, serial, etc)
+  x = 2;
+  y = 1;
 }
 
 void loop() {
+  if(!(angle == 180 && y == 1) && !(angle == 90 && x == 3) && !(angle == -90 && x == 1))
+  {
+    MOTOR_SetSpeed(0, 1);
+	  MOTOR_SetSpeed(1, 1);
+	  delay(1000);
+	  MOTOR_SetSpeed(0, 0);
+	  MOTOR_SetSpeed(1, 0);
+	
+	  if(angle == 0)
+		  y++;
+	  if(angle == 180)
+		  y--;
+	  if(angle == 90)
+		  x++;
+	  if(angle == -90)
+		  x--;
+  }
+  else
+  {
+    bool gauche, droite;
+
+    if(y % 2 == 0)
+	  {
+      MOTOR_SetSpeed(0, 1);
+	    MOTOR_SetSpeed(1, -1);
+	    delay(2000);
+	    MOTOR_SetSpeed(0, 0);
+	    MOTOR_SetSpeed(1, 0);
+
+		  if(angle == 0)
+		  	angle == 180;
+		  else
+		  	angle == 0;
+	  }
+    else
+    {
+      if((angle == 0 && x != 3) || (angle == 180 && x != 1) || (angle == 90 && y != 1))
+		  {
+			  MOTOR_SetSpeed(0, 1);
+	      MOTOR_SetSpeed(1, -1);
+	      delay(1000);
+	      MOTOR_SetSpeed(0, 0);
+	      MOTOR_SetSpeed(1, 0);
   
+			  if(true /*!LEDCapteurObstacle*/)
+			  	droite = true;
+  
+			  MOTOR_SetSpeed(0, -1);
+	      MOTOR_SetSpeed(1, 1);
+	      delay(1000);
+	      MOTOR_SetSpeed(0, 0);
+	      MOTOR_SetSpeed(1, 0);
+		  }
+
+      if((angle == 0 && x != 1) && (angle == 180 && x != 3) || (angle == -90 && y != 1))
+		  {
+		  	MOTOR_SetSpeed(0, -1);
+	      MOTOR_SetSpeed(1, 1);
+	      delay(1000);
+	      MOTOR_SetSpeed(0, 0);
+	      MOTOR_SetSpeed(1, 0);
+
+		  	if(true /*!LEDCapteurObstacle*/)
+		  		gauche = true;
+
+		  	MOTOR_SetSpeed(0, 1);
+	      MOTOR_SetSpeed(1, -1);
+	      delay(1000);
+	      MOTOR_SetSpeed(0, 0);
+	      MOTOR_SetSpeed(1, 0);
+		  }
+
+      if(!droite && !gauche)
+      {
+			  MOTOR_SetSpeed(0, 1);
+	      MOTOR_SetSpeed(1, -1);
+	      delay(2000);
+	      MOTOR_SetSpeed(0, 0);
+	      MOTOR_SetSpeed(1, 0);
+
+			  if(angle == 0)
+			  	angle = 180;
+			  if(angle == 180)
+			  	angle = 0;
+			  if(angle == 90)
+			  	angle = -90;
+			  if(angle == -90)
+			  	angle = 90;
+		  }
+    }
+  }
 }
