30cf4206cb1c5463b95c9b83d25e36482247411d
MAubin <mazacra12345@gmail.com>
2023-09-28 13:53:07 -0400

    Update et correction main

--- PabstPatrouille/src/main.cpp
+++ PabstPatrouille/src/main.cpp
@@ -2,206 +2,163 @@
 
 int x,y,angle;
 
-void setup(){
-  BoardInit();
+void avancer(){
+	MOTOR_SetSpeed(0, 1);
+	MOTOR_SetSpeed(1, 1);
+	delay(1000);
+	MOTOR_SetSpeed(0, 0);
+	MOTOR_SetSpeed(1, 0);
+}
 
-  x = 2;
-  y = 1;
+void tGauche(int iteration = 1){
+	MOTOR_SetSpeed(0, -1);
+	MOTOR_SetSpeed(1, 1);
+	delay(iteration * 1000);
+	MOTOR_SetSpeed(0, 0);
+	MOTOR_SetSpeed(1, 0);
+}
+
+void tDroite(int iteration = 1){
+	MOTOR_SetSpeed(0, 1);
+	MOTOR_SetSpeed(1, -1);
+	delay(iteration * 1000);
+	MOTOR_SetSpeed(0, 0);
+	MOTOR_SetSpeed(1, 0);
+}
+
+void setup(){
+	BoardInit();
+
+  	x = 2;
+  	y = 1;
 }
 
 void loop() {
-  if(!(angle == 180 && y == 1) && !(angle == 90 && x == 3) && !(angle == -90 && x == 1))
-  {
-    MOTOR_SetSpeed(0, 1);
-	  MOTOR_SetSpeed(1, 1);
-	  delay(1000);
-	  MOTOR_SetSpeed(0, 0);
-	  MOTOR_SetSpeed(1, 0);
+	if(!(angle == 180 && y == 1) && !(angle == 90 && x == 3) && !(angle == -90 && x == 1))
+	{
+    	avancer();
 	
-	  if(angle == 0)
-		  y++;
-	  if(angle == 180)
-		  y--;
-	  if(angle == 90)
-		  x++;
-	  if(angle == -90)
-		  x--;
-  }
-  else
-  {
-    bool gauche, droite;
-
-    if(y % 2 == 0)
-	  {
-      MOTOR_SetSpeed(0, 1);
-	    MOTOR_SetSpeed(1, -1);
-	    delay(2000);
-	    MOTOR_SetSpeed(0, 0);
-	    MOTOR_SetSpeed(1, 0);
-
-		  if(angle == 0)
-		  	angle == 180;
-		  else
-		  	angle == 0;
-	  }
-    else
-    {
-      if((angle == 0 && x != 3) || (angle == 180 && x != 1) || (angle == 90 && y != 1))
-		  {
-			  MOTOR_SetSpeed(0, 1);
-	      MOTOR_SetSpeed(1, -1);
-	      delay(1000);
-	      MOTOR_SetSpeed(0, 0);
-	      MOTOR_SetSpeed(1, 0);
+		if(angle == 0)
+			y++;
+	  	if(angle == 180)
+			y--;
+	  	if(angle == 90)
+		  	x++;
+	  	if(angle == -90)
+		 	x--;
+  	}
+  	else
+  	{
+		bool gauche, droite;
+    	if(y % 2 == 0)
+		{
+    		tDroite(2);
+	  		if(angle == 0)
+	  			angle == 180;
+	  		else
+	  			angle == 0;
+		}
+    	else
+    	{
+      		if((angle == 0 && x != 3) || (angle == 180 && x != 1) || (angle == 90 && y != 1))
+		  	{
+			  	tDroite();
   
-			  if(true /*!LEDCapteurObstacle*/)
-			  	droite = true;
+			  	if(true /*!LEDCapteurObstacle*/)
+			  		droite = true;
   
-			  MOTOR_SetSpeed(0, -1);
-	      MOTOR_SetSpeed(1, 1);
-	      delay(1000);
-	      MOTOR_SetSpeed(0, 0);
-	      MOTOR_SetSpeed(1, 0);
-		  }
+			  	tGauche();
+		  	}
 
-      if((angle == 0 && x != 1) && (angle == 180 && x != 3) || (angle == -90 && y != 1))
-		  {
-		  	MOTOR_SetSpeed(0, -1);
-	      MOTOR_SetSpeed(1, 1);
-	      delay(1000);
-	      MOTOR_SetSpeed(0, 0);
-	      MOTOR_SetSpeed(1, 0);
+      		if((angle == 0 && x != 1) && (angle == 180 && x != 3) || (angle == -90 && y != 1))
+		  	{
+		  		tGauche();
 
-		  	if(true /*!LEDCapteurObstacle*/)
-		  		gauche = true;
+		  		if(true /*!LEDCapteurObstacle*/)
+		  			gauche = true;
 
-		  	MOTOR_SetSpeed(0, 1);
-	      MOTOR_SetSpeed(1, -1);
-	      delay(1000);
-	      MOTOR_SetSpeed(0, 0);
-	      MOTOR_SetSpeed(1, 0);
-		  }
+		  		tDroite();
+		  	}
 
-      if(!droite && !gauche)
-      {
-			  MOTOR_SetSpeed(0, 1);
-	      MOTOR_SetSpeed(1, -1);
-	      delay(2000);
-	      MOTOR_SetSpeed(0, 0);
-	      MOTOR_SetSpeed(1, 0);
+      		if(!droite && !gauche)
+      		{
+			  	tDroite(2);
 
-			  if(angle == 0)
-			  	angle = 180;
-			  if(angle == 180)
-			  	angle = 0;
-			  if(angle == 90)
-			  	angle = -90;
-			  if(angle == -90)
-			  	angle = 90;
-		  }
-      if(droite && gauche)
-	  {
-		if(angle == 0||angle == 180||angle == 90)
-			droite=false
-		else 
-		gauche=false
-	  }
+			  	if(angle == 0)
+			  		angle = 180;
+			  	if(angle == 180)
+			  		angle = 0;
+			  	if(angle == 90)
+			  		angle = -90;
+			  	if(angle == -90)
+			  		angle = 90;
+		  	}
 
-	  if (angle == 0)
-	  {
-		if (gauche)
-		{
-			MOTOR_SetSpeed(0,-1);
-			MOTOR_SetSpeed(-1,1);
-			delay(1000);
-			MOTOR_SetSpeed(0,0);
-			MOTOR_SetSpeed(1,0);
+      		if(droite && gauche)
+	  		{
+				if(angle == 0||angle == 180||angle == 90)
+					droite=false;
+				else 
+					gauche=false;
+	  		}
 
-			angle = -90;
-		}
-		if(droite)
-		{
-			MOTOR_SetSpeed(0,1);
-			MOTOR_SetSpeed(1,-1);
-			delay(1000);
-			MOTOR_SetSpeed(0,0);
-			MOTOR_SetSpeed(1,0);
-
-			angle = 90;
-		}		
-	  }
-	  if (angle==180)
-	  {
-		if(gauche)
-		{
-			MOTOR_SetSpeed(0,-1);
-			MOTOR_SetSpeed(-1,1);
-			delay(1000);
-			MOTOR_SetSpeed(0,0);
-			MOTOR_SetSpeed(1,0);
-
-			angle=90;
-		}
-		if (droite)
-		{
-			MOTOR_SetSpeed(0,1);
-			MOTOR_SetSpeed(1,-1);
-			delay(1000);
-			MOTOR_SetSpeed(0,0);
-			MOTOR_SetSpeed(1,0);
-
-			angle = -90;
-		}
-	  }
-	  if(angle==90)
-	  {
-		if(gauche)
-		{
-			MOTOR_SetSpeed(0,-1);
-			MOTOR_SetSpeed(-1,1);
-			delay(1000);
-			MOTOR_SetSpeed(0,0);
-			MOTOR_SetSpeed(1,0);
-
-			angle = -90;
-		}
-		if(droite)
-		{
-			MOTOR_SetSpeed(0,1);
-			MOTOR_SetSpeed(1,-1);
-			delay(1000);
-			MOTOR_SetSpeed(0,0);
-			MOTOR_SetSpeed(1,0);
-
-			angle = 90;
-		}
-	  }
-		if(angle==-90)
-		{
-			if(gauche)
+			switch (angle)
 			{
-				MOTOR_SetSpeed(0,-1);
-				MOTOR_SetSpeed(-1,1);
-				delay(1000);
-				MOTOR_SetSpeed(0,0);
-				MOTOR_SetSpeed(1,0);
-				
-				angle = 180;
-			}
-			if (droite)
-			{
-				MOTOR_SetSpeed(0,1);
-				MOTOR_SetSpeed(1,-1);
-				delay(1000);
-				MOTOR_SetSpeed(0,0);
-				MOTOR_SetSpeed(1,0);
+				case 0:
+					if (gauche)
+					{
+						tGauche();
+						angle -= 90;
+					}
 
-				angle = 0;
-			}
-		}
-	  }
+					if(droite)
+					{
+						tDroite();
+						angle += 90;
+					}		
+					break;
+				case 90:
+					if(gauche)
+					{
+						tGauche();
+						angle -= 90;
+					}
+					if(droite)
+					{
+						tDroite();
+						angle += 90;
+					}
+					break;
+				case -90:
+					if(gauche)
+					{
+						tGauche();
+						angle = 180;
+					}
+					if (droite)
+					{
+						tDroite();
+						angle -= 90;
+					}
+					break;
+				case 180:
+					if(gauche)
+					{
+						tGauche();
+						angle=90;
+					}
+					if (droite)
+					{
+						tDroite();
+						angle = -90;
+					}
+					break;
+				default:
+					break;
+			}	
+	  	}
 
     }   
 
-  }
 }
+
