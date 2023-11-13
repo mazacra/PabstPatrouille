0e7ef373c56ebe76e835a046f350218a1a40502f
MAubin <63813705+mazacra@users.noreply.github.com>
2023-11-13 10:00:58 -0500

    Revert des changements dans main

--- PabstPatrouille/src/main.cpp
+++ PabstPatrouille/src/main.cpp
@@ -2,40 +2,12 @@
 #include <LibRobus.h>
 #include <Adafruit_TCS34725.h>
 
-#define redpin 3
-#define greenpin 5
-#define bluepin 6
-#define commonAnode true
-Adafruit_TCS34725 tcs = Adafruit_TCS34725();
-byte gammatable[256];
-
 void setup() {
 	BoardInit(); //Init libraire LibRobus
     
 	Serial.begin(9600);
-
-  
-  if (tcs.begin()) {
-    Serial.println("Found sensor");
-  } else {
-    Serial.println("No TCS34725 found ... check your connections");
-    while (1);
-  }
 }
 
 void loop() {
-	uint16_t r, g, b, c, colorTemp, lux;
-  
-  tcs.getRawData(&r, &g, &b, &c);
-  colorTemp = tcs.calculateColorTemperature(r, g, b);
-  lux = tcs.calculateLux(r, g, b);
-  
-  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
-  delay(1000);
+	
 }
